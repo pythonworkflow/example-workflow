f3a68b285e5f6938761f3fe2d7f5f79ac9db6556
Jan Janßen <janssen@mpie.de>
2025-04-25 12:37:31 +0200

    update aiida

--- .ci_support/aiida.py
+++ .ci_support/aiida.py
@@ -1,8 +1,9 @@
 from python_workflow_definition.aiida import load_workflow_json
+load_profile()
+
 from aiida import load_profile
 
 
 if __name__ == "__main__":
-    load_profile()
     workgraph = load_workflow_json(file_name='workflow.json')
     workgraph.run()
