80aedf4f0d74e44a5aa97407043d194c00bf4d9d
Jan Janßen <janssen@mpie.de>
2025-04-25 12:39:34 +0200

    reorder imports

--- .ci_support/aiida.py
+++ .ci_support/aiida.py
@@ -1,7 +1,7 @@
-from python_workflow_definition.aiida import load_workflow_json
+from aiida import load_profile
 load_profile()
 
-from aiida import load_profile
+from python_workflow_definition.aiida import load_workflow_json
 
 
 if __name__ == "__main__":
