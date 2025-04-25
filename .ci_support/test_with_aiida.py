1e694e21a80ed2aefc310286f9ea29f260e4d733
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-04-25 13:30:00 +0200

    dynamically create test scripts (#3)
    * dynamically create test scripts
    
    * fixes
    
    * readme
    
    * add publishing instructions

--- .ci_support/test_with_aiida.py
+++
@@ -1,9 +0,0 @@
-from aiida import load_profile
-load_profile()
-
-from python_workflow_definition.aiida import load_workflow_json
-
-
-if __name__ == "__main__":
-    workgraph = load_workflow_json(file_name='workflow.json')
-    workgraph.run()
--- .ci_support/test_with_jobflow.py
+++
@@ -1,7 +0,0 @@
-from jobflow.managers.local import run_locally
-from python_workflow_definition.jobflow import load_workflow_json
-
-
-if __name__ == "__main__":
-    flow = load_workflow_json(file_name="workflow.json")
-    print(run_locally(flow))
